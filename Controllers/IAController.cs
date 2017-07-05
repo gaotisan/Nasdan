355eb23b0663ad8914d371b7eacbedac8d353f60
SantiSATEC <santiago.ochoa@int.satec.es>
2017-07-05 10:56:33 +0200

    v1.0

--- Controllers/IAController.cs
+++ Controllers/IAController.cs
@@ -9,42 +9,11 @@ namespace Nasdan.Controllers
     public class IAController : Controller
     {
         public IActionResult Index()
-        {
-            //Cogeremos el último estimulo recibido y abrieremos esa vista
-            ViewData["IAActive"] = "active";
-            ViewData["BlogActive"] = "";
-            return View("Seeing");
-        }
-        public IActionResult Seeing()
-        {
+        {            
             ViewData["IAActive"] = "active";
             ViewData["BlogActive"] = "";
             return View();
         }
 
-        public IActionResult Terminal()
-        {
-            ViewData["IAActive"] = "active";
-            ViewData["BlogActive"] = "";
-            return View();
-        }
-
-
-        public IActionResult Learning()
-        {
-            ViewData["IAActive"] = "active";
-            ViewData["BlogActive"] = "";
-            return View();
-        }
-
-        public IActionResult Monitoring()
-        {
-            ViewData["IAActive"] = "active";
-            ViewData["BlogActive"] = "";
-            return View();
-        }
-
-
-      
     }
 }
