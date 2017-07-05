c91df2e9cbe5d1e2058519d9152f3a73145651b1
SantiSATEC <santiago.ochoa@int.satec.es>
2017-07-05 09:06:55 +0200

    v1.0

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,5 +1,66 @@
 ﻿// Write your Javascript code.
+$(document)
+  .ajaxStart(function () {
+    $loading.show();
+  })
+  .ajaxStop(function () {
+    $loading.hide();
+});
+
 $('.nav-tabs a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 })
+
+$('.btn-input').click(function(e){
+  $('.btn-input').removeClass('active');
+  $('.img-before.no-clicked').attr("src", "../../images/image-background-before-click-me.gif");
+});
+
+$('.img-before').click(function(e){
+  $img = $('.btn-input.active').find('img');
+  $(this).attr("src",$img.attr('src'));
+  $(this).removeClass('no-clicked');
+  $(this).addClass('clicked');
+  $('.img-after.no-clicked').attr("src", "../../images/image-background-after-click-me.gif");
+  $('#clean1').prop('disabled', false);
+  $('#send1').prop('disabled', false);
+});
+
+$('.img-after').click(function(e){
+  $img = $('.btn-input.active').find('img');
+  $(this).attr("src",$img.attr('src'));
+  $(this).removeClass('no-clicked');
+  $(this).addClass('clicked');
+  $('#clean2').prop('disabled', false);
+  $('#send2').prop('disabled', false);
+});
+
+$('#clean1').click(function(e){
+   $('.img-before').attr("src", "../../images/image-background-before-click-me.gif");
+   $('.img-after').attr("src", "../../images/image-background-after.gif");
+   $('.img-before').removeClass('clicked');
+   $('.img-before').addClass('no-clicked');
+   $('.img-after').removeClass('clicked');
+   $('.img-after').addClass('no-clicked');
+   $('#clean1').prop('disabled', true);
+   $('#send1').prop('disabled', true);
+   $('#clean2').prop('disabled', true);
+   $('#send2').prop('disabled', true);
+});
+
+$('#clean2').click(function(e){  
+   $('.img-after.clicked').attr("src", "../../images/image-background-after-click-me.gif");
+   $('.img-after').removeClass('clicked');
+   $('.img-after').addClass('no-clicked');
+   $('#clean2').prop('disabled', true);
+   $('#send2').prop('disabled', true);
+});
+
+$('#send1').click(function(e){
+
+});
+
+$('#send2').click(function(e){
+
+});
