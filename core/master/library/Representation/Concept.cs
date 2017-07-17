6cb19a49554d9f7dea0f8197c386441d544ac119
Santiago Ochoa <Santi@MacBook-Air-de-Santiago.local>
2017-07-17 07:55:08 +0200

    v1.0

---
+++ core/master/library/Representation/Concept.cs
@@ -0,0 +1,12 @@
+using System;
+using OrientDB.Net.Core;
+using Neo4jClient;
+using System.Linq;
+
+namespace library.Representation
+{
+    public class Concept
+    {
+        string Name {get;set;}
+    }
+}
--- web/master/Controllers/Web/BlogController.cs
+++ web/master/Controllers/Web/BlogController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Nasdan.Controllers
+namespace Nasdan.Controllers.Web
 {
     public class BlogController : Controller
     {
