8840a49bb1ab16a42eb417231b215660434cb7fc
Santiago Ochoa <Santi@MacBook-Air-de-Santiago.local>
2017-07-20 18:46:15 +0200

    v1.0

---
+++ core/master/API/Hello.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Neo4jClient;
+using System.Linq;
+
+namespace library
+{
+    public class Hello
+    {
+        public Person GetTomHanks(){
+            var client = new GraphClient(new Uri("http://127.0.0.1:7474/browser/"),"neo4j", "123");
+            client.Connect();
+            Person tom = client.Cypher.Match("(person:Person)")
+                .Where((Person person) => person.name == "Tom Hanks")
+                .Return(person => person.As<Person>())
+                .Results
+                .Single();
+                
+            return tom;
+        }
+    }
+}
---
+++ core/master/Representation/Concept.cs
@@ -0,0 +1,11 @@
+using System;
+using Neo4jClient;
+using System.Linq;
+
+namespace library.Representation
+{
+    public class Concept
+    {
+        string Name {get;set;}
+    }
+}
