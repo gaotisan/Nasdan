656c65fa2ed91fe2acd0089c5e81a65f25de7d06
Santiago Ochoa <Santi@MacBook-Air-de-Santiago.local>
2017-08-10 19:49:19 +0200

    v1.0

--- core/master/Representation/Cypher.cs
+++ core/master/Representation/Cypher.cs
@@ -4,15 +4,61 @@ using System.Linq;
 using Neo4jClient;
 using Neo4jClient.Cypher;
 using Newtonsoft.Json;
+using System.Runtime.InteropServices;
 
 namespace Nasdan.API.Neo4j
 {
     internal class Cypher
     {
         public GraphClient Client { get; protected set; }
-        public Cypher()
+
+        public enum Representation
         {
-            this.Client = new GraphClient(new Uri("http://localhost.:7474/db/data"), "neo4j", "123");
+            knowloges,
+            experiences,
+        }
+
+        private string _user = "neo4j";
+        private string _password = "123";
+
+        private string _neo4jVersion = "neo4j-3.2.3";
+
+
+        public Cypher(Representation representation)
+        {
+            string uri = this.GetUriServer(representation);
+
+
+
+        }
+
+        public string GetFolderServer(Representation representation, OSPlatform platform)
+        {
+            string result = representation.ToString() + "-" + this._neo4jVersion;
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                result += "-windows";
+            }
+            else
+            {
+                result += "-linux-mac";
+            }
+            return result;
+        }
+
+        public string GetUriServer(Representation representation)
+        {
+            string result = "http://localhost.:7474/db/data";
+            if (representation == Representation.experiences)
+            {
+                result = "http://localhost.:7474/db/data";
+            }
+            return result;
+        }
+
+        public Cypher(string uri, string user, string password)
+        {
+            this.Client = new GraphClient(new Uri(uri), this._user, this._password);
             this.Client.Connect();
         }
 
