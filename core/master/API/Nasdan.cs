cd58d65777038b1a8c4c1c2bc996620b097ee54f
SantiSATEC <santiago.ochoa@int.satec.es>
2017-08-16 10:05:27 +0200

    v1.0

--- core/master/API/Nasdan.cs
+++ core/master/API/Nasdan.cs
@@ -1,6 +1,7 @@
 using Proto;
 using Nasdan.Core.Actors;
 using Nasdan.Core.Senses;
+using Nasdan.API.Neo4j;
 
 namespace Nasdan.Core.API
 {
@@ -38,8 +39,25 @@ namespace Nasdan.Core.API
 
         public static void StartServers()
         {
-            System.Diagnostics.Process.Start("https://www.duckduckgo.com/?q=cat pictures");
+            Nasdan._startProcess(Enviroment.GetFileName(Enviroment.Representation.experiences), Enviroment.GetStartArgument());
+            Nasdan._startProcess(Enviroment.GetFileName(Enviroment.Representation.knowloges), Enviroment.GetStartArgument());
+
+            //Nasdan._startProcess(Nasdan.API.Neo4j.Enviroment., "console");
+            //Nasdan._startProcess(@"C:\Users\santiago.ochoa\Desktop\Nasdan\core\master\knowloges-neo4j-3.2.3-windows\bin\neo4j.bat", "console");
+        }
+
+        protected static void _startProcess(string filename, string arguments){
+            var server = new System.Diagnostics.Process();
+            server.StartInfo.FileName = filename;
+            server.StartInfo.Arguments = arguments;
+            server.Start();
         }
 
     }
+
+
+
+
+
+
 }
--- core/master/Neo4j/Enviroment.cs
+++ core/master/Neo4j/Enviroment.cs
@@ -13,8 +13,9 @@ namespace Nasdan.API.Neo4j
         }
 
         private const string _neo4jVersion = "neo4j-3.2.3";
-        private const string _rootGraphLinuxMacFolder = "C:/Users/Santi/desktop/Nasdan/core/master/";
-        private const string _rootGraphWindowsFolder = "C:/Users/santiago.ochoa/Desktop/Nasdan/core/master/";
+        private const string _rootServerLocation = "C:/Users/Nasdan/core/master/";
+        //private const string _rootGraphLinuxMacFolder = "C:/Users/Santi/desktop/Nasdan/core/master/";
+        //private const string _rootGraphWindowsFolder = "C:/Users/santiago.ochoa/Desktop/Nasdan/core/master/";
 
         public static OSPlatform GetOSPlatform()
         {
@@ -29,7 +30,7 @@ namespace Nasdan.API.Neo4j
             }
             return result;
         }
-        public static string GetFolderServer(Representation representation, OSPlatform platform)
+        public static string GetBinFolderServer(Representation representation)
         {
             string result = representation.ToString() + "-" + Enviroment._neo4jVersion;
             if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
@@ -40,17 +41,38 @@ namespace Nasdan.API.Neo4j
             {
                 result += "-linux-mac";
             }
+            result = Enviroment._rootServerLocation + result + "/bin/";
             return result;
+        }
+        public static string GetFileName(Representation representation, bool pathCompleted = true)
+        {
+            string result = "neo4j";
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                result = "neo4j.bat";
+            }
+            if (pathCompleted)
+            {
+                result = Enviroment.GetBinFolderServer(representation) + result;
+            }
+            return result;
+        }
+
+        public static string GetStartArgument()
+        {
+            return "console";
         }
 
         public static string GetUriServer(Representation representation)
         {
-            string result = "http://localhost.:7474/db/data";
+            string result = "http://localhost.:7373/db/data";
             if (representation == Representation.experiences)
             {
-                result = "http://localhost.:7575/db/data";
+                result = "http://localhost.:7474/db/data";
             }
             return result;
         }
+
+
     }
 }
